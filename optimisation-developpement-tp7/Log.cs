130bd7ea2f01c27c106a2dcacebff9c233762bc9
Salomé <salome.cliquennois@viacesi.fr>
2025-10-03 14:34:38 +0200

    Log

---
+++ optimisation-developpement-tp7/Log.cs
@@ -0,0 +1,9 @@
+﻿namespace optimisation_developpement_tp7
+{
+    public class Log
+    {
+        public long Timestamp { get; set; }
+        public required string UserId { get; set; }
+        public Guid ActionId { get; set; }
+    }
+}
